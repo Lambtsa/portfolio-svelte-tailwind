06fc8d821480d0ddcfe97ef89f0639f66b3c50b1
Lambtsa <42414095+Lambtsa@users.noreply.github.com>
2021-08-01 23:30:30 +0200

    testing setup

---
+++ src/__tests__/index.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/svelte";
+import Stats from '../components/Stats.svelte'
+
+describe("Copyright", () => {
+  test("Sanity test", () => {
+    expect("1").toBe("1")
+  });
+});
