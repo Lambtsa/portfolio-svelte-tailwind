c571df10f30db4055dd42088a187f9b0ad5c76b2
lambtsa <lambtsa.tom@gmail.com>
2022-11-07 20:45:23 +0100

    Update testing and button padding

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,7 +1,7 @@
 // import { render, screen } from "@testing-library/svelte";
 // import Stats from '../components/Stats.svelte';
 
-describe("Copyright", () => {
+describe("Sanity test", () => {
   test("Sanity test", () => {
     expect("1").toBe("1")
   });
--- src/lib/Jumbotron.svelte
+++ src/lib/Jumbotron.svelte
@@ -7,7 +7,7 @@
   <div class="container mx-4 max-w-screen-sm space-y-2 px-4">
     <h1 id="typing" class="font-bold text-5xl text-white">Hello! 👋🏼 I'm Tom!</h1>
     <p class="appear_ease_in_5s text-subtitle">I'm a passionate fullstack javascript developer and product manager based out of Paris, France.</p>
-    <button class="appear_ease_in_5s bg-transparent border mt-4 border-white font-bold hover:bg-green-400 hover:border-green-400 hover:text-black px-8 py-4 rounded-lg text-white" type="button">
+    <button class="appear_ease_in_5s bg-transparent border mt-4 border-white font-bold hover:bg-green-400 hover:border-green-400 hover:text-black px-4 py-2 rounded-lg text-white" type="button">
       <a href="mailto:hello@tom-lamb.com">Contact me</a>
     </button>
   </div>
